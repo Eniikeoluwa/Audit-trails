4cab96f3c4fe4d1b2bf38ada985d95a9a2bf74fa
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-11-16 18:22:25 +0100

    Audit trails

---
+++ src/AuditTrails/Enums/AuditOperation.cs
@@ -0,0 +1,17 @@
+namespace AuditTrails.Enums;
+
+public enum AuditOperation
+{
+    Create,
+    Read,
+    Update,
+    Delete,
+    Login,
+    Logout,
+    AccessDenied,
+    Export,
+    Import,
+    ConfigurationChange,
+    Custom
+}
+
